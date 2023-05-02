473e61f753d87ba69b38b43f521e53172070b2bb
Hieu Le <hieuhuule@gmail.com>
2023-05-01 21:40:30 -0500

    added a table render and headers

--- src/PropertyList.js
+++ src/PropertyList.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect, useMemo } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useMemo } from "react";
+import axios from "axios";
 
-const API_BASE_URL = 'https://api-demo.mlsgrid.com/v2/Property';
-const ACCESS_TOKEN = '19cf3858acb8e0296488848bef6b32379af6b55c';
+const API_BASE_URL = "https://api-demo.mlsgrid.com/v2/Property";
+const ACCESS_TOKEN = "19cf3858acb8e0296488848bef6b32379af6b55c";
 
 const PropertyList = () => {
   const [properties, setProperties] = useState([]);
 
-  const headers = useMemo(() => ({
-    'Authorization': `Bearer ${ACCESS_TOKEN}`,
-  }), [ACCESS_TOKEN]);
+  const headers = useMemo(
+    () => ({
+      Authorization: `Bearer ${ACCESS_TOKEN}`,
+    }),
+    [ACCESS_TOKEN]
+  );
 
   useEffect(() => {
     const fetchProperties = async () => {
@@ -17,7 +20,7 @@ const PropertyList = () => {
         const response = await axios.get(API_BASE_URL, { headers });
         setProperties(response.data.value);
       } catch (error) {
-        console.error('Error fetching properties:', error);
+        console.error("Error fetching properties:", error);
       }
     };
 
@@ -25,16 +28,22 @@ const PropertyList = () => {
   }, [headers]);
 
   return (
-    <div>
-      <h1>Property List</h1>
-      <ul>
+    <table>
+      <thead>
+        <tr>
+          <th>Property ID</th>
+          <th>List Price</th>
+        </tr>
+      </thead>
+      <tbody>
         {properties.map((property, index) => (
-          <li key={index}>
-            {property.PropertyID}: {property.ListPrice}
-          </li>
+          <tr key={index}>
+            <td>{property['@odata.id']}</td>
+            <td>{property['PublicRemarks']}</td>
+          </tr>
         ))}
-      </ul>
-    </div>
+      </tbody>
+    </table>
   );
 };
 
