d25f2f8eb6aa0222c9caa410f45d2d9c8f4354ba
Hieu Le <hieuhuule@gmail.com>
2023-05-03 11:30:19 -0500

    fixed title by removing reference of demo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <title>Acuity - Stellar MLS DEMO KEY</title>
+        <title>ACG - Stellar MLS</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap"
           rel="stylesheet"
@@ -20,7 +20,7 @@ function App() {
           sizes="150x150"
         />
       </Helmet>
-      <h1>Acuity - Stellar MLS DEMO KEY</h1>
+      <h1>ACG - Stellar MLS</h1>
       <PropertyList />
     </div>
   );
